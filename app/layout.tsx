773082e2945b3f3ab130e805aa4dfa84ba9028f1
thief <thiefainedev@gmail.com>
2025-01-16 13:55:59 +0100

    feat(package): add pnpm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+            <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" />
+      </Head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
